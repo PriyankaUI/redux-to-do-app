da8a6336acafa39f4752ac4b7e556d8e5dea9c14
PriyankaUI <66872012+PriyankaUI@users.noreply.github.com>
2021-05-21 16:24:29 +0530

    added

--- src/App.js
+++ src/App.js
@@ -3,16 +3,9 @@ import './App.css';
 import ToDo from './components/ToDo';
 
 function App() {
-  // it alternative to the useContext hooks in react / consumer from context API
-  //const changeTheNumber = useSelector(state => state.changeTheNumber);
-
-  //const dispatch = useDispatch();
-
   return (
     <>
-      <ToDo/>
-       
-     
+      <ToDo/>   
     </>
   )
 
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,41 +1,41 @@
-import React,{useState} from 'react'
-import {  FaPlus,FaRegTrashAlt } from 'react-icons/fa';
+import React, { useState } from 'react'
+import { FaPlus, FaRegTrashAlt } from 'react-icons/fa';
 import { useDispatch, useSelector } from 'react-redux';
 import { addtodo, deltodo, remove } from '../actions';
 
 function ToDo() {
-const [inputdata, setinputdata] = useState('')
-const list=useSelector((state)=>state.todoReducer.list);
-const dispatch=useDispatch();
+    const [inputdata, setinputdata] = useState('')
+    const list = useSelector((state) => state.todoReducer.list);
+    const dispatch = useDispatch();
     return (
         <>
-           <div className="main-div">
-      <div className="child-div">
-        <figure>
-          <figcaption>Add your list here ✋</figcaption>
-        </figure>
-        <div className="addItems">
-          <input type="text" placeholder="✍️  Add items " value={inputdata} onChange={(event)=>setinputdata(event.target.value)} />
-          <FaPlus onClick={()=> dispatch(addtodo(inputdata),setinputdata(''))} />
-        </div>
-        <div className="showitems">
-        {
-            list.map((ele)=>{
-                return <div className="each-item" key={ele.id}>
-                <h3>{ele.data}</h3>
-            <FaRegTrashAlt  title="delete" onClick={()=> dispatch(deltodo(ele.id))} />
-            </div>
-            })
-        }
-            
+            <div className="main-div">
+                <div className="child-div">
+                    <figure>
+                        <figcaption>Add your list here ✋</figcaption>
+                    </figure>
+                    <div className="addItems">
+                        <input type="text" placeholder="✍️  Add items " value={inputdata} onChange={(event) => setinputdata(event.target.value)} />
+                        <div className="icon"><FaPlus onClick={() => dispatch(addtodo(inputdata), setinputdata(''))} /></div>
+                    </div>
+                    <div className="showitems">
+                        {
+                            list.map((ele) => {
+                                return <div className="each-item" key={ele.id}>
+                                    <h3>{ele.data}</h3>
+                                    <div className="icon2"><FaRegTrashAlt title="delete" onClick={() => dispatch(deltodo(ele.id))} /></div>
+                                </div>
+                            })
+                        }
 
-        </div>
-        <div className="showitems">
-            <button className='btn effect'onClick={()=> dispatch(remove(inputdata))} >remove all <span>Checklist</span></button>
-        </div>
-      </div>
-       
-      </div> 
+
+                    </div>
+                    <div className="showitems">
+                        <button className='btn effect' onClick={() => dispatch(remove(inputdata))} >remove all <span>Checklist</span></button>
+                    </div>
+                </div>
+
+            </div>
 
         </>
     )
